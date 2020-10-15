df8daba6d7645251b9ce2590def1375f8b768511
rodriy20 <rodriy20@users.noreply.github.com>
2020-10-14 19:08:49 -0500

    Fix Deprecation Warning

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import connectDatabase from './config/db';
 import { check, validationResult } from 'express-validator';
 import cors from 'cors';
+import bcrypt from 'bcryptjs';
+import User from './models/User';
 
 //Initialize express application
 const app = express();
@@ -42,12 +44,38 @@ app.post(
             'Please enter a password with 6 or more characters')
             .isLength({min: 6})
     ],
-    (req, res) => {
+    async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(422).json({errors: errors.array() });
         } else {
-            return res.send(req.body);
+            const { name, email, password } = req.body;
+            try {
+                //check if user exists
+                let user = await User.findOne({email: email});
+                if (user) {
+                    return res
+                        .status(400)
+                        .json({ errors: [{ msg: 'User already exists' }] });
+                }
+
+                //Create a new user
+                user = new User({
+                    name: name,
+                    email: email,
+                    password: password
+                });
+
+                //Encrypt the password
+                const salt = await bcrypt.genSalt(10);
+                user.password = await bcrypt.hash(password, salt);
+
+                //Save to the db and return
+                await user.save();
+                res.send('User successfully registered');
+            } catch (error) {
+                res.status(500).send('Server error');
+            }
         }
     }
 );
