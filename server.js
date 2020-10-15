b27f9a768d0ad0b5695d99aedd45ae53e89be681
rodriy20 <rodriy20@users.noreply.github.com>
2020-10-14 19:47:28 -0500

    Added JWT code

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import connectDatabase from './config/db';
 import { check, validationResult } from 'express-validator';
 import cors from 'cors';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import config from 'config';
 import User from './models/User';
 
 //Initialize express application
@@ -72,7 +74,23 @@ app.post(
 
                 //Save to the db and return
                 await user.save();
-                res.send('User successfully registered');
+                
+                //Generate and return a JWT token
+                const payload = {
+                    user: {
+                        id: user.id
+                    }
+                };
+
+                jwt.sign(
+                    payload,
+                    config.get('jwtSecret'),
+                    { expiresIn: '10hr' },
+                    (err, token) => {
+                        if (err) throw err;
+                        res.json({ token: token });
+                    }
+                );
             } catch (error) {
                 res.status(500).send('Server error');
             }
