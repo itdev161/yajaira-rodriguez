99c17a2d8c0be56e7c37e71e59e5cc79d948c576
rodriy20 <rodriy20@users.noreply.github.com>
2020-12-01 15:29:56 -0600

    Endpoint to delete post

--- server.js
+++ server.js
@@ -240,6 +240,34 @@ app.get('/api/posts/:id', auth, async (req, res) => {
   }
 });
 
+/**
+ * @route DELETE api/posts/:id
+ * @desc Delete a post
+ */
+app.delete('/api/posts/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Make sure the post was found
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    // Make sure the request user created the post
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+
+    await post.remove();
+
+    res.json({ msg: 'Post removed' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
+  }
+});
+
+
 // Connection listener
 const port = 5000;
 app.listen(port, () => console.log(`Express server running on port ${port}`));
