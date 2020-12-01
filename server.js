651fa5da6732df0ecebac2c23d4b7bd4ad69231d
rodriy20 <rodriy20@users.noreply.github.com>
2020-12-01 15:33:15 -0600

    Endpoint to Update Post

--- server.js
+++ server.js
@@ -267,6 +267,37 @@ app.delete('/api/posts/:id', auth, async (req, res) => {
   }
 });
 
+/**
+ * @route PUT api/posts/:id
+ * @desc Update a post
+ */
+app.put('/api/posts/:id', auth, async (req, res) => {
+  try {
+    const { title, body } = req.body;
+    const post = await Post.findById(req.params.id);
+
+    // Make sure the post was found
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    // Make sure the request user created the post
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+
+    // Update the post and return
+    post.title = title || post.title;
+    post.body = body || post.body;
+
+    await post.save();
+
+    res.json(post);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
+  }
+});
 
 // Connection listener
 const port = 5000;
